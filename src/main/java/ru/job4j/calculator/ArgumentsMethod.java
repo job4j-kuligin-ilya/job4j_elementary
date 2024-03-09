d2d8d8a0493532f675211ffffb0f192017639dfd
job4j-kuligin-ilya <job4j.kuligin.ilya@gmail.com>
2024-03-09 23:30:19 +0300

    4.1. Аргументы

---
+++ src/main/java/ru/job4j/calculator/ArgumentsMethod.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculator;
+
+public class ArgumentsMethod {
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        for (int i = 0; i < 4; i++) {
+            ArgumentsMethod.hello(name, age);
+        }
+    }
+}
